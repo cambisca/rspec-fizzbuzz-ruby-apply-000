ef79d7b41a65dc028c6bf9e95160fd820678ad6f
Cameron Bisca <cjbisca@gmail.com>
2020-10-02 22:19:24 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,6 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz(int)
-  if int / 3 == 3  
-    puts "Fizz"
-  elsif int / 5 == 5  
-    puts "Buzz"
-  elsif int / 3 || 5 == 1 
-    puts "FizzBuzz"
-  end
+def fizzbuzz
+
 end
 
